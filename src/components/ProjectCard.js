061bb34138d3aa29e38b2487d86cdc259a087722
n1shaant007 <nishanthipparagi6@gmail.com>
2024-01-04 21:01:31 +0530

    first commit

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,8 +7,8 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
       <div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+          <h4 className="projectHoverTitle">{title}</h4>
+          <span className="projectHoverSpan">{description}</span>
         </div>
       </div>
       </Link>
