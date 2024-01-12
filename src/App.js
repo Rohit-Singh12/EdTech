c702ed0f31d1e0193fd720b79c2947d36f3d45df
n1shaant007 <nishanthipparagi6@gmail.com>
2024-01-12 18:51:16 +0530

    login page first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router, Route, Routes
 } from "react-router-dom";
 import DescriptionComponent from "./pages/DescriptionComponent";
+import FormPage from "./pages/FormPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
        <Routes>
           <Route path="/" element={<Introduction />} />
           <Route path="/Description" element={<DescriptionComponent />} />
+          <Route path="/login" element={<FormPage />} />
         </Routes>
    </Router>
   );
